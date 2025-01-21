0f0625c2645f8561d5ab1125768965ed368811cb
juk3b0x <jest00004@htwsaar.de>
2025-01-21 16:04:48 +0000

    added mob skeleton

--- mob.lua
+++ mob.lua
@@ -1,3 +1,36 @@
 mob = {
+    dmg = 10
+    mspeed = 1
+    hp = 100
+    x = 0
+    y = 0
+    sprites = {1,2}
+    function move(movdir)
+        if movdir == "u" then
+            y = y + mspeed
+        elseif movdir == "d" then
+            y = y - mspeed
+        elseif movdir == "l" then
+            x = x - mspeed
+        else
+            x = x + mspeed
+        end
+    end
+    function attack()
+        --attack player for dmg
+    end
+    function die()
+        --despawn object and sprite
+        --give player gold
+    end
+    function spawn()
+        --spawn object and sprite
+    end
+    function patrol()
+        --patrol between two points
+    end
+    function chase()
+        --chase player
+    end
     
 }
